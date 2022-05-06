c86682d5bd48862999938a0d20b05a979f15d74c
Your Name <100946724+ryodmm@users.noreply.github.com>
2022-05-06 09:02:35 +0000

    [add]bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-class ApplicationController < ActionController::
-  
+class ApplicationController < ActionController::Base
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -7,5 +7,5 @@ class ApplicationController < ActionController::
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
+
 end
