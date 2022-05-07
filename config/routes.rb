c8a435f29586c7d157e007874805009f89317584
Your Name <100946724+ryodmm@users.noreply.github.com>
2022-05-07 06:36:55 +0000

    [add_login_name]boookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  devise_for :users
+
+  devise_for :user
+
+
   get "/homes/about" => "homes#about", as: "about"
 
-  resources :users, only:[:show, :edit]
-  resources :books, only:[:new, :index, :show, :edit]
+  resources :users
+  resources :books
 
 
 end
