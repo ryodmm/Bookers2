115697c1b6f5cdbd752c1b732dfb734ae919d2f1
Your Name <100946724+ryodmm@users.noreply.github.com>
2022-05-11 05:52:20 +0000

    [add_bookedit_btnbookers2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :introduction, presence: false, length: { maximum: 50 }
 
   has_one_attached :profile_image
   def get_profile_image(width, height)
